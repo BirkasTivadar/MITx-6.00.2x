fa935fbca217638a11bd57e3b191d88b3a10c4ea
BirkasTivadar <birkastivadar@gmail.com>
2023-04-12 23:30:37 +0200

    Lecture 2 - Decision Trees and Dynamic Programming weigthed edge, cost path

---
+++ UNIT1/Lecture2/costPath.py
@@ -0,0 +1,69 @@
+from graph import *
+from path import *
+
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight=0):
+        Edge.__init__(self, src, dest)
+        self.weigth = weight
+
+    def getWeight(self):
+        return self.weigth
+
+    def __str__(self):
+        return Edge.__str__(self) + ' ({})'.format(self.weigth)
+
+
+def cost(path):
+    result = 0
+    for i in range(len(path)):
+        result += str(path[i])
+        if i != len(path) - 1:
+            result += '->'
+    return result
+
+
+def DFS(graph, start, end, path, shortest, toPrint=False):
+    """Assumes graph is a Digraph; start and end are nodes;
+          path and shortest are tuples containing a list of
+          nodes and a cost
+       Returns a shortest path from start to end in graph"""
+    path = (path + [start], 0)
+    if toPrint:
+        print('Current DFS path:', printPath(path[0]))
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path:  # avoid cycles
+            if shortest == None or cost(path) < cost(shortest):
+                newPath = DFS(graph, node, end, path, shortest,
+                              toPrint)
+                if newPath != None:
+                    shortest = newPath
+
+
+def testSP():
+    nodes = []
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'):  # Create 6 nodes
+        nodes.append(Node(str(name)))
+    g = DirectedGraph()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(WeightedEdge(nodes[0], nodes[1]))
+    g.addEdge(WeightedEdge(nodes[1], nodes[2]))
+    g.addEdge(WeightedEdge(nodes[2], nodes[3]))
+    g.addEdge(WeightedEdge(nodes[2], nodes[4]))
+    g.addEdge(WeightedEdge(nodes[3], nodes[4]))
+    g.addEdge(WeightedEdge(nodes[3], nodes[5]))
+    g.addEdge(WeightedEdge(nodes[0], nodes[2], 10))
+    g.addEdge(WeightedEdge(nodes[1], nodes[0]))
+    g.addEdge(WeightedEdge(nodes[3], nodes[1]))
+    g.addEdge(WeightedEdge(nodes[4], nodes[0]))
+    sp = shortestPath(g, nodes[0], nodes[5], toPrint=True)
+    print('Shortest path is', printPath(sp))
+    sp = BFS(g, nodes[0], nodes[5])
+    print('Shortest path found by BFS:', printPath(sp))
+
+
+testSP()
